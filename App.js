536bba60d56bbccc408028fff81fa8b333d8bfde
satish1348 <114284127+satish1348@users.noreply.github.com>
2023-07-29 15:55:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Calc from './components/Calc';
+import './components/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calc />
+    </div>
+  );
+}
+
+export default App;
